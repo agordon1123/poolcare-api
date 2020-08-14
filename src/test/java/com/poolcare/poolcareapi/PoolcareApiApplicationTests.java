cfa44825adc1dfebc0813b279d577139839ee559
Alex <alex.gordon@optum.com>
2020-08-13 21:26:02 -0400

    created new spring-boot app

---
+++ src/test/java/com/poolcare/poolcareapi/PoolcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poolcare.poolcareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolcareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
